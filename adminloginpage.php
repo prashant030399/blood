59d69dd697eb48c8f1fb550cc5145fbd3de7de53
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2025-01-28 14:44:05 +0530

    Update adminloginpage.php

--- adminloginpage.php
+++ adminloginpage.php
@@ -4,6 +4,7 @@
                 <meta charset="UTF_8">
                 <meta name="viewport" content="width-device-width, initial-scale=1.0">
                 <title>Document</title>
+
     <style>
         *
         {
